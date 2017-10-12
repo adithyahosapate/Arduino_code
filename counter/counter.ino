5831ed20731469887a32986ee7ac1c9af9cc5008
adithyahosapate <adihosapate@gmail.com>
2017-10-12 16:25:22 +0530

    added files:

--- counter/counter.ino
+++ counter/counter.ino
@@ -1,7 +1,11 @@
 int a,b,c,d,e,f,g;
-int A,B,C,D;
+
+
+int pin1=0;
+int A=0,B=0,C=0,D=0;
 void setup()
 {
+  pinMode(0,OUTPUT);
   pinMode(1,OUTPUT);
   pinMode(2,OUTPUT);
   pinMode(3,OUTPUT);
@@ -10,25 +14,37 @@ void setup()
   pinMode(6,OUTPUT);
   pinMode(7,OUTPUT);
     pinMode(8,OUTPUT);
- Serial.begin(9600);      // open the serial port at 9600 bps:    
+ //Serial.begin(9600);      // open the serial port at 9600 bps:    
   pinMode(9,INPUT);
   pinMode(10,INPUT);
   pinMode(11,INPUT);
   pinMode(12,INPUT);
-
+  pinMode(13,OUTPUT);
   
   
   
 }
-int led=8;
+int led=8,temp;
 void loop()
 {
- 
-   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);               // wait for a second
+
+  
+   digitalWrite(0,0);
+
+
+
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(10);  
+  
+
+
+
+
+
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);               // 
-  A=digitalRead(9);
+  //delay();               // 
+   
+       A=digitalRead(9);
   B=digitalRead(10);
   C=digitalRead(11);
   D=digitalRead(12);
@@ -36,25 +52,47 @@ void loop()
   Serial.print(B);
   Serial.print(C);
   Serial.print(D);
+  
+  Serial.print("\n");
 
-  a = (!A && B && !C && !D) || (!A && !B && !C && D) || (A && B && C && !D) || (A && !B && C && D);
+
+a = (!A && B && !C && !D) || (!A && !B && !C && D) || (A && B && C && !D) || (A && !B && C && D);
 b = (!A && B && !C && D) || (!A && B && C && !D) || (A && B && !C && !D) || (A && B && C && D);
 c = (!A && !B && C && !D);
 d = (!B && !C && D) || (!A && B && !C && !D) || (A && !B && D) || (!A && B && C && D) || (A && B && C && !D);
 e = (D) | (!A && B && !C) || (A && B && C);
-f = (!B && C) || (!A && B && D) || (D && !A && !B) || (!C && A && B);
-g = (A && B  && C && D) || (!A && !B && !C) || (A && !B && C);
- digitalWrite(1,a);
- digitalWrite(2,b);
- digitalWrite(3,c);
- digitalWrite(4,d);
- digitalWrite(5,e);
- digitalWrite(6,f);
- digitalWrite(7,g);
+f = (!B && C&& D) || (!A && !B && D) || (D && !A && C) || (C &&!A && !B)|| (!C &&A && B);
+if (A==1&&(B==1||C==1))
+{
+ writenum(0,1,1,0,0,0,0,1);
+ 
+}
+else
+{
+ writenum(0,0,0,0,0,0,1,1);
+ 
+}
 
-    
 
 
+g = (!A && B  && C && D) || (!A && !B && !C) || (A && !B && C);
+
+if(A==1 && B==1 &&C==0 &&D==0)
+{
+  temp=b;
+  b=c;
+  c=temp;
+}
+writenum(a,b,c,d,e,f,g,0);
+
+   Serial.print(a);
+  Serial.print(b);
+  Serial.print(c);
+  Serial.print(d);
+    Serial.print(e);
+  Serial.print(f);
+  Serial.print(g);
+   Serial.print("\n");
 
 
  
@@ -62,7 +100,24 @@ g = (A && B  && C && D) || (!A && !B && !C) || (A && !B && C);
 
 
 
+void writenum(int a,int b,int c,int d,int e,int f,int g,int pin)
+{
+  for (int i=0;i<10;i++)
+{
+  digitalWrite(pin,1);
+  digitalWrite(!pin,0);
 
+ digitalWrite(13,a);
+ digitalWrite(2,b);
+ digitalWrite(3,c);
+ digitalWrite(4,d);
+ digitalWrite(5,e);
+ digitalWrite(6,f);
+ digitalWrite(7,g);
+ delay(50);
+}
+  
+}
 
 
 
